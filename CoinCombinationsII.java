925cd926ab01a6be61417fa7deb5718d7857cf7f
yash621 <yg17381@gmail.com>
2021-08-20 21:29:43 +0530

    solved

---
+++ CoinCombinationsII.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CoinCombinationsII {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] v = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            v[i] = sc.nextInt();
+        }
+          
+        long[] dp = new long[n+1];
+        dp[]
+
+
+
+
+
+
+
+    }
+
+}
